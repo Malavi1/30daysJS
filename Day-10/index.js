a300b13a123a4981f2c6cbb2ff5c7d8e536409b1
Malavi1 <pandemalavi7@gmail.com>
2023-12-11 07:31:31 +0530

    completed

--- Day-10/index.js
+++ Day-10/index.js
@@ -1,10 +1,17 @@
 const QRinput = document.getElementById("QR-input");
 const QRImg = document.getElementById("QR-img");
-const QRBtn = document.getElementById("QR-btn");
-
+console.log(QRinput.value.length);
+console.log(QRinput);
 function QRimage() {
-  QRImg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    QRinput.value;
+  console.log(QRinput.value.length);
+  if (QRinput.value.length > 0) {
+    QRImg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      QRinput.value;
+    QRImg.classList.add("show-img");
+  } else {
+    QRinput.classList.add("error");
+  }
 }
-QRimage();
+
+QRinput.value = "";
